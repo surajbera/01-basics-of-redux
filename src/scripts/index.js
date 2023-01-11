3f69c64b4bfc68068a90e8fea16021481972dd1a
suraj.be <suraj.be@media.net>
2023-01-11 20:50:34 +0530

    implemented redux toolkit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ const unSubscribe = store.subscribe(() => {
   console.log('Store Got Updated: ', store.getState())
 })
 
-store.dispatch(addTask('Task 1'))
-store.dispatch(addTask('Task 2'))
-store.dispatch(removeTask(1))
-store.dispatch(completeATask(2))
+store.dispatch(addTask({ task: 'Task 1' }))
+store.dispatch(addTask({ task: 'Task 2' }))
+store.dispatch(removeTask({ id: 1 }))
+store.dispatch(completeATask({ id: 2 }))
 
--- src/scripts/store/tasks.js
+++ src/scripts/store/tasks.js
@@ -1,27 +1,32 @@
+import { createAction } from '@reduxjs/toolkit'
+
 /* Action Types */
 export const ADD_TASK = 'ADD_TASK'
 export const REMOVE_TASK = 'REMOVE_TASK'
 export const COMPLETE_A_TASK = 'COMPLETE_A_TASK'
 
 /* Actions */
-export const addTask = (task) => {
-  return { type: ADD_TASK, payload: { task } }
-}
+// export const addTask = (task) => {
+//   return { type: ADD_TASK, payload: { task } }
+// }
 
-export const removeTask = (id) => {
-  return { type: REMOVE_TASK, payload: { id } }
-}
+// export const removeTask = (id) => {
+//   return { type: REMOVE_TASK, payload: { id } }
+// }
 
-export const completeATask = (id) => {
-  return { type: COMPLETE_A_TASK, payload: { id } }
-}
+// export const completeATask = (id) => {
+//   return { type: COMPLETE_A_TASK, payload: { id } }
+// }
+export const addTask = createAction('ADD_TASK')
+export const removeTask = createAction('REMOVE_TASK')
+export const completeATask = createAction('COMPLETE_A_TASK')
 
 /* Reducer */
 let id = 0
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case ADD_TASK:
+    case addTask.type:
       return [
         ...state,
         {
@@ -31,10 +36,10 @@ export default function reducer(state = [], action) {
         },
       ]
 
-    case REMOVE_TASK:
+    case removeTask.type:
       return state.filter((task) => task.id !== action.payload.id)
 
-    case COMPLETE_A_TASK:
+    case completeATask.type:
       return state.map(task => task.id === action.payload.id ? {
         ...task,
         completed: true
