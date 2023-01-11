43c3400c0fc8259164d6fe00a495145b01604bc7
suraj.be <suraj.be@media.net>
2023-01-11 21:18:24 +0530

    Slice really makes it easy, now there is no need to add actionTypes, actions, reducers

--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
 - Reducer is a pure function which means how to do
+  
 - Deprecated means this feature is supported but not recommended
+  
 - Root reducer is a combination of all reducers
+  
 - store.subscribe runs when something changes in the redux store
+  
 - There's no need to import extra package for enabling 'Redux Devtools' while using 'Redux Toolkit'
+  
 - configureStore also allow us to dispatch asynchronous actions
+  
+- Actions can be created using redux toolkit using createAction
+  
+- Reducers can be created using redux toolkit using createReducer
+
+- Code inside createReducer looks mutable, but under the hood, immer library is responsible for transforming that mutable code into immutable format
--- src/scripts/store/tasks.js
+++ src/scripts/store/tasks.js
@@ -1,51 +1,28 @@
-import { createAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-/* Action Types */
-export const ADD_TASK = 'ADD_TASK'
-export const REMOVE_TASK = 'REMOVE_TASK'
-export const COMPLETE_A_TASK = 'COMPLETE_A_TASK'
-
-/* Actions */
-// export const addTask = (task) => {
-//   return { type: ADD_TASK, payload: { task } }
-// }
-
-// export const removeTask = (id) => {
-//   return { type: REMOVE_TASK, payload: { id } }
-// }
-
-// export const completeATask = (id) => {
-//   return { type: COMPLETE_A_TASK, payload: { id } }
-// }
-export const addTask = createAction('ADD_TASK')
-export const removeTask = createAction('REMOVE_TASK')
-export const completeATask = createAction('COMPLETE_A_TASK')
-
-/* Reducer */
 let id = 0
 
-export default function reducer(state = [], action) {
-  switch (action.type) {
-    case addTask.type:
-      return [
-        ...state,
-        {
-          id: ++id,
-          task: action.payload.task,
-          completed: false,
-        },
-      ]
-
-    case removeTask.type:
-      return state.filter((task) => task.id !== action.payload.id)
-
-    case completeATask.type:
-      return state.map(task => task.id === action.payload.id ? {
-        ...task,
-        completed: true
-      } : task)
-
-    default:
-      return state
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false
+      })
+    },
+    removeTask: (state, action) => {
+      const index = state.findIndex(task => task.id === action.payload.id)
+      state.splice(index, 1)
+    },
+    completeATask: (state, action) => {
+      const index = state.findIndex(task => task.id === action.payload.id)
+      state[index].completed = true
+    }
   }
-}
+})
+
+export const { addTask, removeTask, completeATask } = taskSlice.actions
+export default taskSlice.reducer
